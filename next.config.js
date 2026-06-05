4c412e53e82d134e7ffecaa7bb223760795af0d3
FarerABR <a.baghaee81@outlook.com>
2026-06-06 01:21:16 +0330

    initial commit

--- next.config.js
+++ next.config.js
@@ -1,1 +1,8 @@
-module.exports = {};
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  basePath: '', // e.g. '/LiveTerm' or '' if using username.github.io
+  images: { unoptimized: true },
+};
+
+module.exports = nextConfig;
