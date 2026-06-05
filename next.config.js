46aadd5055ac5fcc64c8e6e2cf469f2f5681f20e
FarerABR <a.baghaee81@outlook.com>
2026-06-06 01:36:46 +0330

    fixed next.config

--- next.config.js
+++ next.config.js
@@ -1,8 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  basePath: '', // e.g. '/LiveTerm' or '' if using username.github.io
   images: { unoptimized: true },
-};
+}
 
-module.exports = nextConfig;
+module.exports = nextConfig
+
+
